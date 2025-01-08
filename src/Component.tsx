6017f051f57bfbe2663b3b36913dd7f07491f8ae
Zhenqian He <hezhenqian@gmail.com>
2025-01-08 10:29:53 -0800

    update

--- src/Component.tsx
+++ src/Component.tsx
@@ -3,12 +3,13 @@ import React from "react";
 
 function ChildComponent() {
   const { ready, wallets } = useSolanaWallets();
-  const { login } = usePrivy();
+  const { login, ready: privyReady } = usePrivy();
 
   return (
     <div>
       <button onClick={() => login()}>Login</button>
-      <div>ready: {ready ? "true" : "false"}</div>
+      <div>useSolanaWallets ready: {ready ? "true" : "false"}</div>
+      <div>usePrivy Ready: {privyReady ? "true" : "false"}</div>
       <div>
         wallets:{" "}
         {wallets.length ? wallets.map((wallet) => wallet.address) : "none"}
